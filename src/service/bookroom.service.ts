cfc171ddad78b8a83f0ae405975798d7efc18a8f
DOTRUONGBACH <vuabongdapro@gmail.com>
2023-03-27 02:30:59 +0700

    update sent mail khi book room

--- src/service/bookroom.service.ts
+++ src/service/bookroom.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {BillDetail} from "../app/model/BillDetail";
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {BillDTO} from "../app/model/BillDTO";
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ import {HttpClient} from "@angular/common/http";
 export class BookroomService {
 
   constructor(private http: HttpClient) {}
-  sendEmailbooking(roomId:number, bd: BillDetail):Observable<any>{
-    return this.http.post<any>('http://localhost:8080/billdetails/emailemailbooking?roomId='+roomId,bd)
+  sendEmailbooking(accountId: number,bill: BillDTO ):Observable<any>{
+    return this.http.post<any>('http://localhost:8080/billdetails/emailbooking/'+accountId,bill)
   }
 }
